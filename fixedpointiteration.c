35823f54750103773d360e9e206db3e1049a62b1
Mishan Ghimire <mishanghimire2003@gmail.com>
2024-01-15 18:18:24 +0545

    final

--- fixedpointiteration.c
+++ fixedpointiteration.c
@@ -14,7 +14,7 @@ double g(double x)
 int main()
 {
     double x0, x1, f1, e;
-    int SN = 1;
+    int SN = 1, max_iterations = 10; // Set a maximum iteration limit
 
     printf("\nThis CODE IS RUN BY  MISHAN GHIMIRE\n");
     printf("Enter the error criteria (e): ");
@@ -25,14 +25,16 @@ int main()
 
     printf("S.N\t x0\t x1\t f(x1)\n");
 
-    while (fabs(f(x1)) > e)
+    x1 = g(x0);
+
+    while (fabs(f(x1)) > e && SN <= max_iterations)
     {
-        x1 = g(x0);
         f1 = f(x1);
 
         printf("%d\t%lf\t%lf\t%lf\n", SN, x0, x1, f1);
 
         x0 = x1;
+        x1 = g(x0);
         SN++;
     }
 
--- newtonraphson.c
+++ newtonraphson.c
@@ -16,14 +16,16 @@ int main()
     double x0, x1, f0, f1, g0, e;
     int SN = 1;
 
-    printf("\nThis CODE IS RUN BY  MISHAN GHIMIRE\n");
+    printf("\nThis CODE IS RUN BY MISHAN GHIMIRE\n");
     printf("Enter the error criteria (e): ");
     scanf("%lf", &e);
 
     printf("Enter the initial guess (x0): ");
     scanf("%lf", &x0);
 
-    printf("S.N\tx0\t\t\t x1\t\t\t f(x0)\t\t f(x1)\n");
+    printf("S.N\t x0\t\t x1\t\t f(x0)\t\t f(x1)\n");
+
+    x1 = x0 - (f(x0) / g(x0)); 
 
     while (fabs(f(x1)) > e)
     {
